0e12f46d8a10328d86b8183c54268f02f5b6e0c1
Corey Arnold <coreyarnold@gmail.com>
2024-05-04 19:41:11 -0700

    update save topics to store in sqlite and mariadb. add some error checking

--- sensor_storage/save_topic_data.py
+++ sensor_storage/save_topic_data.py
@@ -1,20 +1,50 @@
 import sqlite3
+import mysql.connector
 import json
 import os
+from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
+load_dotenv()
+def getEnvVar(token_name):
+    token = os.getenv(token_name)
+    if token == None:
+        print (f"Environment variable {token_name} not set. Exiting.")
+        exit(1)
+    return token
+
+print("connecting to db and getting schema in place if not exists")
 # SQLite database setup
 conn = sqlite3.connect('IoT.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS MQTT_MESSAGES
              (timestamp DATETIME, topic TEXT, message TEXT)''')
 
+print("reading environment variables")
 # MQTT broker settings
-broker_address = os.environ["MQTT_BROKER"]
-port = int(os.environ["MQQT_BROKER_PORT"])
-username = os.environ["MQTT_USER"]
-password = os.environ["MQTT_PASS"]
+broker_address = getEnvVar("MQTT_BROKER")
+port = int(getEnvVar("MQQT_BROKER_PORT"))
+username = getEnvVar("MQTT_USER")
+password = getEnvVar("MQTT_PASS")
+
+# MariaDB
+# MariaDB database details
+db_host = getEnvVar("MYSQL_HOST")
+db_user = getEnvVar("MYSQL_USER")
+db_password = getEnvVar("MYSQL_PASSWORD")
+db_name  = getEnvVar("MYSQL_DB")
+table_name  = getEnvVar("MYSQL_TABLE")
+
+# Connect to MariaDB
+db_connection = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
+)
+db_cursor = db_connection.cursor()
+
 
 # Callback when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
@@ -23,8 +53,20 @@ def on_connect(client, userdata, flags, rc):
 
 # Callback when a message is received from the broker
 def on_message(client, userdata, msg):
-    print("Received message on topic: " + msg.topic)
+    print("Received message" + str(msg.payload) + " on topic: " + msg.topic)
+#    saveToMariaDB(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg.topic, msg.payload.decode(), msg.payload.decode('utf-8'))
+    
+    saveToSQLite(msg)
 
+# Function to insert data into MariaDB table
+def saveToMariaDB(timestamp, topic, payload, raw_message):
+    sql = f"INSERT INTO {table_name} (timestamp,topic, message_value, message) VALUES (%s, %s, %s, %s)"
+    val = (timestamp, topic, payload, raw_message)
+    db_cursor.execute(sql, val)
+    db_connection.commit()
+    print("Data inserted into MariaDB table")
+
+def saveToSQLite(msg):
     message_data = {
         'topic': msg.topic,
         'message': msg.payload.decode('utf-8'),
@@ -32,9 +74,8 @@ def on_message(client, userdata, msg):
     }
     c.execute("INSERT INTO MQTT_MESSAGES (topic, message, timestamp) VALUES (?, ?, ?)", (msg.topic, json.dumps(message_data), message_data['timestamp']))
     conn.commit()
-        
 
-
+print("setting up mqtt client")
 # MQTT client setup
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -42,5 +83,6 @@ client.on_message = on_message
 client.username_pw_set(username, password)
 client.connect(broker_address, port, 60)
 
+print("and away we go...")
 # Start the MQTT client loop
 client.loop_forever()
